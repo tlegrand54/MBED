dae1370848e8bb5774ddbc013ad888559dc128e5
tlegrand <legrand.thomas@outlook.fr>
2014-10-14 13:28:37 +0200

    Premier code avec une mbed

---
+++ CodeTest/main.cpp
@@ -0,0 +1,15 @@
+#include "mbed.h"
+
+DigitalOut myled(LED1);
+
+int main()
+{
+    while(1)
+    {
+        myled = 1;
+        wait(0.2);
+        myled = 0;
+        wait(0.2);
+    }
+    return 0;
+}
