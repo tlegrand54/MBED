72cc468666247e40cb0161507c0c87364d4e962e
tlegrand <legrand.thomas@outlook.fr>
2015-05-14 17:16:56 +0200

    AI for testing motor, rotation and sensors

--- Robot2015/TestAI.cpp
+++ Robot2015/TestAI.cpp
@@ -12,7 +12,7 @@ TestAI::~TestAI() {
 
 
 void TestAI::init() {
-	printf("je suis là\n");
+	printf("\n\rTESTS TESTS TESTS");
 }
 
 void TestAI::start(Match* match) {
@@ -20,21 +20,73 @@ void TestAI::start(Match* match) {
 }
 
 void TestAI::run() {
-	printf("mise en route des moteurs\n");
+	printf("\n\r\n\r=== MOTOR TESTS ===\n\r");
 
 	robot.setRotation(0);
 
+	printf("\n\r=> +1.0");
+	robot.setMoveSpeed(+1.0);
+	wait(2);
+
+	printf("\n\r=> +0.5");
+	robot.setMoveSpeed(+0.5);
+	wait(2);
+
+	printf("\n\r=> +0");
+	robot.setMoveSpeed(0);
+	wait(2);
+
+	printf("\n\r=> -0.5");
 	robot.setMoveSpeed(-0.5);
+	wait(2);
 
-	wait(5);
-
-	printf("stop des moteurs\n");
-
-	robot.setRotation(300);
+	printf("\n\r=> -1.0");
+	robot.setMoveSpeed(-1.0);
+	wait(2);
 
 	robot.setMoveSpeed(0);
+	printf("\n\r\n\r=== ROTATION TESTS ===");
 
-	wait(5);
+	printf("\n\r=> 0");
+	robot.setRotation(0);
+	wait(2);
+
+	printf("\n\r=> 60");
+	robot.setRotation(60);
+	wait(2);
+
+	printf("\n\r=> 150");
+	robot.setRotation(150);
+	wait(2);
+
+	printf("\n\r=> 220");
+	robot.setRotation(220);
+	wait(2);
+
+	printf("\n\r=> 300");
+	robot.setRotation(300);
+	wait(2);
+
+	robot.setRotation(0);
+	printf("\n\r\n\r=== SENSOR TESTS ===");
+
+	printf("\n\r=> Front");
+	for(int i = 0; i < 5; i++) {
+		if(robot.detectFrontOpponent())
+			printf("\n\rEnemy detected");
+		else
+			printf("\n\rNo enemy detected");
+		wait(2);
+	}
+
+	printf("\n\r=> Back");
+	for(int i = 0; i < 5; i++) {
+		if(robot.detectBackOpponent())
+			printf("\n\rEnemy detected");
+		else
+			printf("\n\rNo enemy detected");
+		wait(2);
+	}
 }
 
 void TestAI::end() {
